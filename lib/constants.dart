a400b32d7a277f5e8c404dfbabcb8a7ff656579a
eu4ria <aaronjamesrothwell@gmail.com>
2022-09-30 17:43:48 +0100

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  // Colours
+  final int primaryColour = 0xFFF48FB1;
+  final int backgroundColour = 0xFFCCCCCC;
+
+  final int borderColour = 0xFF000000;
+
+  final double boxHeight = 100.0;
+  final double paddingSize = 20.0;
+  final double borderRadius = 6.0;
+
+  // Strings
+  final String appTitle = ". . : : B A S E   A P P : : . .";
+}
